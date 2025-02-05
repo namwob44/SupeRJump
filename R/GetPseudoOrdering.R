3e9a374a1b319b66eb644d73f5d6456af97e29c7
namwob44 <michael.bowman44@gmail.com>
2025-02-04 19:46:09 -0500

    Update GetPseudoOrdering.R corrected gsub to handle dashes in gene names

--- R/GetPseudoOrdering.R
+++ R/GetPseudoOrdering.R
@@ -14,7 +14,8 @@ GetPseudoOrdering<-function(sce,supervised_list_of_list,name_of_score){
   if(class(supervised_list_of_list)=="list"){
     for(iter in 1:(length(supervised_list_of_list))){
       sce<-Seurat::AddModuleScore(sce,features=
-                                    list("score"=c(gsub("(?<=\\b)([a-z])", "\\U\\1", (supervised_list_of_list[[iter]]), perl=TRUE))),
+                                    #list("score"=c(gsub("(?<=\\b)([a-z])", "\\U\\1", (supervised_list_of_list[[iter]]), perl=TRUE))),
+                                  list("score"=c(gsub("((?<=\\b.)[[:upper:]])", "\\L\\1", (supervised_list_of_list[[iter]]), perl=TRUE))),
                                   name=paste0("score",iter),
                                   search=TRUE,)
       sce@meta.data[,dim(sce@meta.data)[2]]<-sce@meta.data[,dim(sce@meta.data)[2]]-min(sce@meta.data[,dim(sce@meta.data)[2]]) # this will make lowest value 0.
@@ -22,7 +23,8 @@ GetPseudoOrdering<-function(sce,supervised_list_of_list,name_of_score){
     }
   }else{
     sce<-Seurat::AddModuleScore(sce,features=
-                                  list("score"=c(gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(supervised_list_of_list), perl=TRUE))),
+                                  #list("score"=c(gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(supervised_list_of_list), perl=TRUE))),
+                                   list("score"=c(gsub("((?<=\\b.)[[:upper:]])", "\\L\\1", (supervised_list_of_list[[iter]]), perl=TRUE))),
                                 name=as.character(name_of_score),search = TRUE)
     sce@meta.data[,dim(sce@meta.data)[2]]<-sce@meta.data[,dim(sce@meta.data)[2]]-min(sce@meta.data[,dim(sce@meta.data)[2]]) # this will make lowest value 0.
   }
