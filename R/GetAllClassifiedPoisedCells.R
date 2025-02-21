56fe0710107080fb0f869319d31c3a0d68930a7f
namwob44 <michael.bowman44@gmail.com>
2025-02-21 16:11:35 -0500

    Update GetAllClassifiedPoisedCells.R
    Updated to get lineages and outlier scores too

--- R/GetAllClassifiedPoisedCells.R
+++ R/GetAllClassifiedPoisedCells.R
@@ -9,24 +9,28 @@
 #' @examples
 GetAllClassifiedPoisedCells<-function(data_input,lineages_to_compare){
   
-  namevector <- paste0("Poised_",lineages_to_compare)
+  # let's create placeholder columns of interest, along with the outlier scores
+  namevector <- paste0("Biased_",lineages_to_compare)
   data_input[ , namevector] <- NA
-  
-  for(cell_iter in 1:length(as.character(unique(data_input$Cluster.Assignment)))){
-    for(iter2 in 1:length(lineages_to_compare)){
-      #for(iter in 1:length(lineages_to_compare)){
+  namevector <- paste0("Outlier_Score_",lineages_to_compare)
+  data_input[ , namevector] <- NA
+  namevector <- paste0("Outlier_Score_loss_",lineages_to_compare)
+  data_input[ , namevector] <- NA
+
+  for(cluster_iter in 1:length(as.character(unique(data_input$Cluster.Assignment)))){
+    for(lineage_iter in 1:length(lineages_to_compare)){
       
-      if(dim(data_input%>%dplyr::filter(Cluster.Assignment==as.character(unique(data_input$Cluster.Assignment))[cell_iter]))[1]>1){
-        
-        poised_vector<-ClassifyPoisedCellsSingleLineage(data_input%>%dplyr::filter(Cluster.Assignment==as.character(unique(data_input$Cluster.Assignment))[cell_iter]),
-                                                        c(lineages_to_compare[iter2]))
-        
-        print(colnames(poised_vector)[(dim(poised_vector)[2]-2):dim(poised_vector)[2]])
-        
-        colnames(poised_vector)[dim(poised_vector)[2]]<-paste0("Poised_",as.character(lineages_to_compare[iter2]),"_",as.character(lineages_to_compare[iter2]))
-        data_input[rownames(poised_vector),paste0("Poised_",as.character(lineages_to_compare[iter2]))]<-poised_vector[,dim(poised_vector)[2]]
-      }
-      #}
+        current_group=as.character(unique(data_input$Cluster.Assignment))[cluster_iter]
+        poised_vector<-ClassifyPoisedCellsSingleLineage(data_subset = data_input%>%
+                                                      filter(Cluster.Assignment==as.character(unique(data_input$Cluster.Assignment))[cluster_iter]),
+                                                    c(lineages_to_compare[lineage_iter],
+                                                      paste0(current_group,"_membership"),
+                                                      paste0(lineages_to_compare[lineage_iter],"_membership")))
+      colnames(poised_vector)
+      data_input[rownames(poised_vector),paste0("Biased_",as.character(lineages_to_compare[lineage_iter]))]<-poised_vector[,"Biased"]
+      data_input[rownames(poised_vector),paste0("Outlier_Score_loss_",as.character(lineages_to_compare[lineage_iter]))]<-poised_vector[,"Outlier_Score_losses"]
+      data_input[rownames(poised_vector),paste0("Outlier_Score_",as.character(lineages_to_compare[lineage_iter]))]<-poised_vector[,"Outlier_Score"]
+          
     }
   }
   return(data_input)
